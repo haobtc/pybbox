b80086e691792157a25fb5a81b266a2c50e43fb5
Zeng Ke <superisaac.ke@gmail.com>
2019-04-26 20:50:47 +0800

    v0.0.7, upgrade version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='pybbox',
-      version='0.0.5',
+      version='0.0.7',
       description="bbox client in python",
       author='Zeng Ke',
       author_email='superisaac.ke@gmail.com',
